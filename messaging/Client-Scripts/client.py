71432a50bbb867b9426ac8df584b9c33964e001d
Calstar9000 <Calstar9000+github@gmail.com>
2025-05-27 14:12:06 -0600

    Update client.py

--- messaging/Client-Scripts/client.py
+++ messaging/Client-Scripts/client.py
@@ -1,3 +1,6 @@
+"""DOES NOT WORK ATM"""
+
+
 # to-do:
 # chatrooms
 # user to user chats
@@ -50,7 +53,7 @@ class HttpClient:
 
 if __name__ == '__main__':
     # use your Cloudflare tunnel address here
-    client = HttpClient('https://stellar.mnasarchive.org', poll_interval=2.0)
+    client = HttpClient('', poll_interval=2.0)
     try:
         while True:
             text = input("Enter message (or 'quit'): ")
